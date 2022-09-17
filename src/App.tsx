e69064c611e0afe154fc88d769dafca397a884b2
Ildus Gafiev <cooltechni@gmail.com>
2022-09-17 15:33:53 +0300

    succesful test

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const themeContext = useContext(ThemeContext);
   console.log(themeContext.darkTheme);
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <ThemeContextProvider>
         <ThemeContext.Consumer>
           {(context: Partial<ThemeContextState>) => (
